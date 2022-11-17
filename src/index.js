3b0ba221c96f3d8cd7942e602dea135a4b7c1e0c
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-11-17 16:40:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -73,14 +73,6 @@ function fetchHits() {
      return loadMoreBtn.hide();
 }
   });
-    const { height: cardHeight } = document
-    .querySelector('.gallery')
-   .firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-   top: cardHeight * 2,
-  behavior: 'smooth',
- });
- lightbox.refresh();
 
 }
 
